e830df2e164230bd8a13bff39b4df52ba273d428
MN Industries <130522379+FoundationINCCorporateTeam@users.noreply.github.com>
2024-05-11 16:43:36 -0500

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,9 @@
+// Redirect function
+function redirectToUntilNextYear() {
+    window.location.href = "/untilnextyear";
+}
+
+// Call the redirect function when the page loads
+window.onload = function() {
+    redirectToUntilNextYear();
+};
