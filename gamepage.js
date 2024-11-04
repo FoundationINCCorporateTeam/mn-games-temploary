d9e17f306358408203a16d48cd3203b0ba394c02
MN Industries <130522379+FoundationINCCorporateTeam@users.noreply.github.com>
2024-11-04 14:19:05 -0600

    Update gamepage.js

--- gamepage.js
+++ gamepage.js
@@ -99,7 +99,7 @@ function openSecureBrowser7() {
     openSecureBrowser(urls7);
 }
 function openSecureBrowser8() {
-    var urls8 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/FoundationINCCorporateTeam%203kh0-assets%20fixy%20motox3m/index.html';
+    var urls8 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/motox3m/motox3m/index.html';
     openSecureBrowser(urls8);
 }
 function openSecureBrowser9() {
@@ -131,39 +131,39 @@ function openSecureBrowser15() {
     openSecureBrowser(urls9);
 }
 function openSecureBrowser16() {
-    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/FoundationINCCorporateTeam/basketball-stars/basketball-stars/index.html';
+    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/basketball-stars/basketball-stars/index.html';
     openSecureBrowser(urls9);
 }
 function openSecureBrowser17() {
-    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/FoundationINCCorporateTeam%20/bit-planes/bit-planes/index.html';
+    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/bit-planes/bit-planes/index.html';
     openSecureBrowser(urls9);
 }
 function openSecureBrowser18() {
-    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/FoundationINCCorporateTeam/core-ball/core-ball/index.html';
+    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/core-ball/core-ball/index.html';
     openSecureBrowser(urls9);
 }
 function openSecureBrowser19() {
-    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/FoundationINCCorporateTeam%20drive-mad/drive-mad/index.html';
+    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/drive-mad/drive-mad/index.html';
     openSecureBrowser(urls9);
 }
 function openSecureBrowser20() {
-    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/FoundationINCCorporateTeam%20impossiblequiz/impossiblequiz/index.html';
+    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/impossiblequiz/impossiblequiz/index.html';
     openSecureBrowser(urls9);
 }
 function openSecureBrowser21() {
-    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/FoundationINCCorporateTeam%20selenite-old%20main%20monkeymart/index.html';
+    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/timeshooter2/timeshooter2/index.html';
     openSecureBrowser(urls9);
 }
 function openSecureBrowser22() {
-    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/FoundationINCCorporateTeam%20selenite-old%20main%20monkeymart/index.html';
+    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/timeshooter3/timeshooter3/index.html';
     openSecureBrowser(urls9);
 }
 function openSecureBrowser23() {
-    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/FoundationINCCorporateTeam%20selenite-old%20main%20monkeymart/index.html';
+    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/9007199254740992/9007199254740992/index.html';
     openSecureBrowser(urls9);
 }
 function openSecureBrowser24() {
-    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/FoundationINCCorporateTeam/MNLimited/main/tunnel-rush/tunnel-rush/index.html';
+    var urls9 = 'https://lively-wave-02deb9a1e.5.azurestaticapps.net/tunnel-rush/tunnel-rush/index.html';
     openSecureBrowser(urls9);
 }
 
